168fab6510571b17c3a42b2d273f3fafdfbac890
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2024-01-20 12:31:23 +0100

    Template dal layout

---
+++ laravel-comics/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends("layouts.initial_layout")
+
+@section("header")
+@include("partials.header")
+@endsection
+
+@section("main")
+@include("partials.main")
+@endsection
+
+@section("footer")
+@include("partials.footer")
+@endsection
